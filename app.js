96017f2269ed1d9e1309c585eec50a22e9bf923b
syedbadhusha <syedbadhusha21@gmail.com>
2024-05-10 18:50:11 +0530

    change cors url

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const urlRouter = require('./routes/urlRouter');
 const app = express();
-const whitelist = ['http://localhost:5173','https://urlsortner-backend-z4gv.onrender.com']
+const whitelist = ['http://localhost:5173','https://luxury-queijadas-04da4f.netlify.app']
 app.use(cors(
     {
         origin(reqOrigin, callback){
